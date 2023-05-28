5ec9e1febf263b8c10b8f6665047197be445b129
Mr_Kmall I <kemal52987@gmail.com>
2023-05-28 14:11:29 +0700

    group 8

---
+++ stock-service/src/test/java/com/group8/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group8.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
